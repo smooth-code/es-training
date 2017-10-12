e335882eddaf7fcb5810fccbdeb51f2cee0f175a
Greg Bergé <berge.greg@gmail.com>
2017-10-12 14:00:11 +0000

    [ex-7] prototype

--- hello-world.js
+++ hello-world.js
@@ -19,4 +19,32 @@ Object.defineProperty(dateUtil, 'help', {
   enumerable: false,
 })
 
-dateUtil.help()
+function Hello(name) {
+  this.name = name
+}
+
+Hello.prototype.say = function say() {
+  return 'hello ' + this.name + '!'
+}
+
+function HelloDate() {
+  Hello.apply(this, arguments)
+}
+
+HelloDate.prototype = Object.create(Hello.prototype)
+HelloDate.prototype.constructor = HelloDate
+
+HelloDate.prototype.say = function say() {
+  var result = Hello.prototype.say.call(this)
+  return (
+    result +
+    ' ' +
+    dateUtil.getTime(date) +
+    ' (' +
+    dateUtil.getEllapsedMinutes(date) +
+    ' min)'
+  )
+}
+
+const helloDate = new HelloDate('world')
+console.log(helloDate.say())
