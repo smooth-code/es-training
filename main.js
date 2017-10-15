e9b594ef3a39d720773ddae3ff2a67b54994c529
Greg Bergé <berge.greg@gmail.com>
2017-10-15 12:43:15 +1100

    [ex-14] movie & string

--- main.js
+++ main.js
@@ -12,4 +12,16 @@ rateForm.addEventListener('submit', function(event) {
   } else {
     alert('NOK')
   }
+
+  var movie = rateForm.movie.value
+
+  if (movie.startsWith('007')) {
+    new Audio(
+      'http://mobile.twilight3g.com/mp3tones/rtones/new/tv-movie/preview/bond_007_original-4821.mp3',
+    ).play()
+  }
+
+  if (movie.toLowerCase().includes('dark')) {
+    document.body.style.backgroundColor = 'black'
+  }
 })
