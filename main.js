e2cdd729457f65b2195e59485036271ae8be2488
Greg Bergé <berge.greg@gmail.com>
2017-10-15 13:24:38 +1100

    [ex-16] alert & template strings

--- main.js
+++ main.js
@@ -6,14 +6,16 @@ rateForm.addEventListener('submit', function(event) {
   event.preventDefault()
 
   const rate = Number(rateForm.rate.value)
-
-  if (Number.isInteger(rate) && rate >= 1 && rate <= 5) {
-    alert('OK')
-  } else {
-    alert('NOK')
-  }
-
+  const alert = rateForm.querySelector('.alert')
   const movie = rateForm.movie.value
+  const valid = Number.isInteger(rate) && rate >= 1 && rate <= 5
+
+  alert.style.display = 'block'
+  alert.classList.toggle('alert-danger', !valid)
+  alert.classList.toggle('alert-success', valid)
+  alert.innerHTML = valid
+    ? `The movie ${movie} has been rated ${rate}!`
+    : `Invalid rate ${rate}.`
 
   if (movie.startsWith('007')) {
     new Audio(
