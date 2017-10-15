bfed4264115d7e72e45051b90d12b420a315c387
Greg Bergé <berge.greg@gmail.com>
2017-10-15 13:12:53 +1100

    [ex-10] webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin()],
+}
