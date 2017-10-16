28dae25572ea4e755e4bdc9b229e4408977a8556
Greg Bergé <berge.greg@gmail.com>
2017-10-17 03:47:57 +1100

    [ex-27] arrays

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 import MovieReview from './MovieReview'
 import { setElementStyle } from './domUtil'
 
+const MOVIE_ALREADY_EXIST = Symbol('already-exist')
+const reviews = []
+
+function validateMovieExistence(review) {
+  if (reviews.some(({ raw: { movie } }) => movie === review.raw.movie)) {
+    return MOVIE_ALREADY_EXIST
+  }
+
+  return null
+}
+
 const delay = duration => new Promise(resolve => setTimeout(resolve, duration))
 
 async function loadHeader() {
@@ -28,7 +39,7 @@ rateForm.addEventListener('submit', event => {
   const review = new MovieReview({ rate, movie, buzzwords })
 
   const alert = rateForm.querySelector('.alert')
-  const invalidReason = review.validate()
+  const invalidReason = review.validate() || validateMovieExistence(review)
 
   function getMessage() {
     switch (invalidReason) {
@@ -40,17 +51,28 @@ rateForm.addEventListener('submit', event => {
         return `Invalid rate ${rate}.`
       case MovieReview.INVALID_BUZZ_WORDS:
         return `Invalid buzz word ${buzzwords}`
+      case MOVIE_ALREADY_EXIST:
+        return 'Movie already exists'
     }
 
     return `The movie ${movie} has been rated ${rate}!`
   }
 
   setElementStyle(alert, { display: 'block' })
+
   alert.classList.toggle('alert-danger', invalidReason)
   alert.classList.toggle('alert-success', !invalidReason)
   alert.innerHTML = invalidReason
     ? getMessage(invalidReason)
     : `The movie ${movie} has been rated ${rate}!`
+
+  Array.from(document.querySelectorAll('.form-control')).forEach(formGroup =>
+    formGroup.classList[invalidReason ? 'add' : 'remove']('is-invalid'),
+  )
+
+  if (!invalidReason) {
+    reviews.push(review)
+  }
 
   delay(2000).then(() => setElementStyle(alert, { display: 'none' }))
 
