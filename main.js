7de7751fdd895faecd15df0beb2fba1051b908f1
Greg Bergé <berge.greg@gmail.com>
2018-01-05 11:13:28 +0100

    [ex-2] variables

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('Hello world!')
+function showReview(title, rate) {
+  let qualifier
+  if (rate === 5) {
+    qualifier = 'amazing'
+  } else if (rate <= 4 && rate >= 3) {
+    qualifier = 'good'
+  } else {
+    qualifier = 'bad'
+  }
+
+  return console.log(title + ' - ' + qualifier)
+}
+
+showReview('The return of Jedi', 5)
+showReview('XxX', 2)
+showReview('Coco', 4)
